e063a4feee1073dac4d9e8aad4e887acacbb9f26
Mikedee-254 <mwangimike15@yahoo.com>
2022-09-15 20:45:31 +0300

    updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="c-footer">
     <div>{!! settings()->footer_text !!}</div>
 
-    <div class="mfs-auto d-md-down-none">Powered by&nbsp;<a href="https://laravel.com" target="_blank">Laravel</a></div>
+    <div class="mfs-auto d-md-down-none">Powered by&nbsp;<a href="https://dukapaq.co" target="_blank">Dukapaq Inc</a></div>
 </footer>
