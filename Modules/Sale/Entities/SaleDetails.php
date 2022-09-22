f5b1359cedbaa43ac78cb969b4e1ebdcd40a5e0a
Mikedee-254 <mwangimike15@yahoo.com>
2022-09-22 20:14:41 +0300

    added sales details with default

--- Modules/Sale/Entities/SaleDetails.php
+++ Modules/Sale/Entities/SaleDetails.php
@@ -15,7 +15,7 @@ class SaleDetails extends Model
     protected $with = ['product'];
 
     public function product() {
-        return $this->belongsTo(Product::class, 'product_id', 'id');
+        return $this->belongsTo(Product::class, 'product_id', 'id')->withDefault();
     }
 
     public function sale() {
